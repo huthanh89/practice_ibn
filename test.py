2db34dbd73391b5a53fab139f3570640376dbbc8
Thanh Huynh <hu.thanh89@yahoo.com>
2019-03-13 03:16:37 -0700

    asdf

--- test.py
+++ test.py
@@ -2,29 +2,40 @@
 # Imports
 # -----------------------------------------------------------------------------
 
-# Library for NETCONF clients. Offer API to map XML to Python construct.
+# Library for NETCONF clients. Offer API to query the device.
 
 from ncclient import manager
 from pprint import pprint
+import xmltodict
 
-HOST = "ios-xe-mgmt.cisco.com"
-PORT = 10000
-USER = "root"
-PASS = "D_Vay!_10&"
+# -----------------------------------------------------------------------------
+# Application
+# -----------------------------------------------------------------------------
+
+HOST_ADDRESS = "ios-xe-mgmt.cisco.com"
+NETCONF_PORT = 10000
+USERNAME = "root"
+PASSWORD = "D_Vay!_10&"
 
 # SSH into device.
 
-with manager.connect(host=HOST, port=PORT, username=USER, password=PASS, hostkey_verify=False, allow_agent=False, look_for_keys=False) as device:
+with manager.connect(host=HOST_ADDRESS, 
+    port=NETCONF_PORT, 
+    username=USERNAME,
+    password=PASSWORD, 
+    hostkey_verify=False, 
+    allow_agent=False, 
+    look_for_keys=False) as deviceManager:
 
-    pprint(device)
-    pprint(device.connected)
-    pprint(device.session_id)
+    pprint(deviceManager)
+    pprint(deviceManager.connected)
+    pprint(deviceManager.session_id)
 
-    xmlResult = device.get_config(source='running').data_xml
+    xmlResult = deviceManager.get_config(source='running').data_xml
 
-    print(device.data_xml)
+    print(deviceManager.data_xml)
 
     # Write result into xml file.
 
-    with open("%s.xml" % HOST, 'w') as f:
+    with open("%s.xml" % HOST_ADDRESS, 'w') as f:
         f.write(xmlResult)
